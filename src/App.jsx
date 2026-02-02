e61f7be9087ed82473f87286abb5873d04d808c1
Sahejdeep Singh <deepsahejs@gmail.com>
2026-02-02 00:53:59 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -562,7 +562,6 @@ const App = () => {
       { name: 'Abhi Ravulaparthy', role: 'Outreach', image: '/data/team/member11.jpg', initials: 'AR', rookie: true },
       { name: 'Kavin Murugan', role: 'Outreach', image: '/data/team/kavin.jpg', initials: 'KM', rookie: true },
       { name: 'Arshaan Husain', role: 'Outreach', image: '/data/team/member13.jpg', initials: 'AH', rookie: true },
-      { name: 'Trisha Chauhan', role: 'Outreach', image: '/data/team/trisha.jpg', initials: 'TC', rookie: true },
       { name: 'Kaiden Lee', role: 'Outreach', image: '/data/team/kaiden.jpg', initials: 'KL', rookie: true },
       { name: 'Jivansh Pandya', role: 'Outreach', image: '/data/team/Jivansh.jpg', initials: 'JP', rookie: true },
     ],
